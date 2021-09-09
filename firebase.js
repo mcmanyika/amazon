87c50df54406cf55ec1b75064aa8ffd984835cec
mcmanyika <mcmanyika.com>
2021-09-09 09:44:13 -0500

    added checkout

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDFAtHLqJz4xlzukcE8d11ezOPEpkPbsBg",
+    authDomain: "amzn-2-7163d.firebaseapp.com",
+    projectId: "amzn-2-7163d",
+    storageBucket: "amzn-2-7163d.appspot.com",
+    messagingSenderId: "97367545170",
+    appId: "1:97367545170:web:9edf34f7d02e43a6d4568b"
+  };
